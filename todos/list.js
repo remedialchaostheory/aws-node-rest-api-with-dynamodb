cde3074cf307a0ee6771aafeeb849e35a0e299e8
remedialchaostheory <remedialchaostheory@users.noreply.github.com>
2019-10-22 21:13:52 -0700

    Edit format and add comments

--- todos/list.js
+++ todos/list.js
@@ -5,29 +5,30 @@
 
   const dynamoDb = new AWS.DynamoDB.DocumentClient();
   const params = {
-    TableName: process.env.DynamoDB,
+    TableName: process.env.DYNAMODB_TABLE,
   };
 
   module.exports.list = (event, context, callback) => {
     dynamoDb.scan(params, (error, result) => {
+      // Handle errors
       if (error) {
         console.error(error);
         callback(null, {
           statusCode: error.statusCode,
-          headers: { 'Content-Type': 'text/plain' },
+          headers: {'Content-Type': 'text/plain'},
           body: 'Couldn\'t get todo list',
         });
+        return;
       }
-      return;
+
+      // Get todo list
+      const response = {
+        statusCode: 200,
+        body: JSON.stringify((result.Items)),
+      };
+
+      callback(null, response);
     });
-
-    const response = {
-      statusCode: 200,
-      body: JSON.stringify((result.Items)),
-    };
-
-    callback(null, response);
   };
-
 
 })();
