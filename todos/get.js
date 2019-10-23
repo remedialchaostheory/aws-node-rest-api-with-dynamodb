122a8712e58dc1841c06affe9c6bd31cdc1299dc
remedialchaostheory <remedialchaostheory@users.noreply.github.com>
2019-10-22 21:39:55 -0700

    Add GET single item request

---
+++ todos/get.js
@@ -0,0 +1,41 @@
+(function () {
+  'use strict';
+
+  const AWS = require('aws-sdk');
+
+  const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+  module.exports.get = (event, context, callback) => {
+
+    console.error('event', event);
+
+    const params = {
+      TableName: process.env.DYNAMODB_TABLE,
+      Key: {
+        id: event.pathParameters.id,
+      },
+    };
+
+    dynamoDb.get(params, (error, result) => {
+      // Handle errors
+      if (error) {
+       console.error(error);
+       callback(null, {
+         statusCode: error.statusCode,
+         headers: { 'Content-Type': 'text/plain' },
+         body: 'Couldn\'t retrieve todo item',
+       });
+       return;
+      }
+
+      // Response
+      const response = {
+        statusCode: 200,
+        body: JSON.stringify(result.item),
+      };
+
+      callback(null, response);
+    });
+  };
+
+})();
