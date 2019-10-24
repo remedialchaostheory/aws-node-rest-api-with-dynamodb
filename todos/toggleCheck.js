d97679991c3ae7089c7494132021acb100fb5318
remedialchaostheory <remedialchaostheory@users.noreply.github.com>
2019-10-24 16:09:05 -0700

    Fix bug where "false" value in PUT method returned an error and move toggleCheckStatus() declaration to top

--- todos/toggleCheck.js
+++ todos/toggleCheck.js
@@ -5,64 +5,7 @@
   const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
   module.exports.toggleCheck = (event, context, callback) => {
-
     const eventData = JSON.parse(event.body);
-
-    // Check for empty data
-    if (!eventData.checked) {
-      callback(null, {
-        statusCode: 400,
-        headers: { 'Content-Type': 'text/plain', },
-        body: 'No event data'
-      });
-      return;
-    }
-
-    // Check for valid data type
-    if (typeof eventData.checked !== 'boolean') {
-      console.log('eventData ->', eventData);
-      console.log('eventData.checked ->', eventData.checked);
-      console.error('Invalid eventData');
-      callback(null, {
-        statusCode: 400,
-        headers: { 'Content-Type': 'text/plain' },
-        body: 'Invalid data type',
-      });
-      return;
-    }
-
-    // Get id's current checked state
-    const ddbParams = {
-      TableName: process.env.DYNAMODB_TABLE,
-      Key: {
-        id: event.pathParameters.id,
-      },
-    };
-
-    const todoItemReq = dynamoDb.get(ddbParams, (error, result) => {
-      // Handle errors
-      if (error) {
-       console.error(error);
-       callback(null, {
-         statusCode: error.statusCode,
-         headers: { 'Content-Type': 'text/plain' },
-         body: 'Couldn\'t retrieve todo item',
-       });
-       return;
-      }
-    });
-
-    todoItemReq.on('success', (response) => {
-      console.log('resp.data.item ->', response.data.Item);
-      console.log('resp.data.item.checked ->', response.data.Item.checked);
-      let data = response.data.Item;
-      // Toggle checked status
-      data.checked = !data.checked;
-      console.log('data.checked after ->', data.checked);
-      toggleCheckStatus(data, callback);
-    });
-
-    // --------------------------------
 
     function toggleCheckStatus(data, callback) {
       const timeStamp = new Date().getTime();
@@ -104,6 +47,54 @@
       });
 
     }
+
+    // Check for empty data
+    if (eventData.checked === '' || eventData.checked === null || eventData.checked === undefined) {
+      callback(null, {
+        statusCode: 400,
+        headers: { 'Content-Type': 'text/plain', },
+        body: 'No event data'
+      });
+      return;
+    }
+
+    // Check for valid data type
+    if (typeof eventData.checked !== 'boolean') {
+      callback(null, {
+        statusCode: 400,
+        headers: { 'Content-Type': 'text/plain' },
+        body: 'Invalid data type',
+      });
+      return;
+    }
+
+    // Get todo item
+    const ddbParams = {
+      TableName: process.env.DYNAMODB_TABLE,
+      Key: {
+        id: event.pathParameters.id,
+      },
+    };
+    dynamoDb.get(ddbParams, (error, result) => {
+      // Handle errors
+      if (error) {
+       console.error(error);
+       callback(null, {
+         statusCode: error.statusCode,
+         headers: { 'Content-Type': 'text/plain' },
+         body: 'Couldn\'t retrieve todo item',
+       });
+       return;
+      }
+    }).on('success', (response) => {
+      console.log('resp.data.item ->', response.data.Item);
+      console.log('resp.data.item.checked ->', response.data.Item.checked);
+      let data = response.data.Item;
+      // Toggle checked status
+      data.checked = !data.checked;
+      console.log('data.checked after ->', data.checked);
+      toggleCheckStatus(data, callback);
+    });
 
     };
 
