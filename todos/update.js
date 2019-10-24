4bbac0f98d13874818d181f935cdc4915cb064b1
remedialchaostheory <remedialchaostheory@users.noreply.github.com>
2019-10-24 12:54:27 -0700

    Add separate code to check for data.check

--- todos/update.js
+++ todos/update.js
@@ -9,12 +9,11 @@
     const timeStamp = new Date().getTime();
     const data = JSON.parse(event.body);
 
-    console.log('event.body ->', event.body);
     console.log('data ->', data);
 
     console.log('typeof data.text ->', typeof data.text);
 
-    // Handle error
+    // Check for valid text
     if (typeof data.text !== 'string') {
       console.log('data ->', data);
       console.log('data.text ->', data.text);
@@ -27,6 +26,10 @@
         body: 'Couldn\'t update todo item',
       });
       return;
+    }
+    // Update text
+    function updateText(data) {
+
     }
 
     // DynamoDB parameters
@@ -48,6 +51,62 @@
 
     // Update item
     dynamoDb.update(params, (error, result) => {
+      if (error) {
+        console.error(error);
+        callback(null, {
+          statusCode: error.statusCode,
+          headers: {
+            'Content-Type': 'text/plain',
+          },
+          body: 'Couldn\'t retrieve todo item'
+        });
+        return;
+      }
+
+      const response = {
+        statusCode: 200,
+        body: JSON.stringify(result.Attributes),
+      };
+
+      callback(null, response);
+    });
+
+    // Update check status
+    // Check for valid "check"
+    if (typeof data.checked !== 'boolean') {
+      console.log('data ->', data);
+      console.log('data.checked ->', data.checked);
+      console.error('Invalid data');
+      callback(null, {
+        statusCode: 400,
+        headers: {
+          'Content-Type': 'text/plain'
+        },
+        body: 'Couldn\'t update todo item',
+      });
+      return;
+    }
+    // Update checked
+    function updatechecked(data) {
+
+    }
+
+    // DynamoDB parameters
+    const checked_params = {
+      TableName: process.env.DYNAMODB_TABLE,
+      Key: {
+        id: event.pathParameters.id,
+      },
+      ExpressionAttributeValues: {
+        ':checked': data.checked,
+        ':updatedAt': timeStamp,
+      },
+      UpdateExpression: 'SET checked = :checked, updatedAt = :updatedAt',
+      ReturnValues: 'ALL_NEW',
+    };
+
+    // Update item
+    dynamoDb.update(checked_params, (error, result) => {
       if (error) {
         console.error(error);
         callback(null, {
