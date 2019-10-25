0b42cf9dae7bcba27c66a0535a4d25000a592649
remedialchaostheory <remedialchaostheory@users.noreply.github.com>
2019-10-24 17:14:39 -0700

    Add id/text id/checked paths

--- todos/update.js
+++ todos/update.js
@@ -11,121 +11,122 @@
 
     console.log('data ->', data);
 
-    console.log('typeof data.text ->', typeof data.text);
-
-    // Check for valid text
-    if (typeof data.text !== 'string') {
-      console.log('data ->', data);
-      console.log('data.text ->', data.text);
-      console.error('Invalid data');
-      callback(null, {
-        statusCode: 400,
-        headers: {
-          'Content-Type': 'text/plain'
-        },
-        body: 'Couldn\'t update todo item',
-      });
-      return;
-    }
-    // Update text
-    function updateText(data) {
-
-    }
-
-    // DynamoDB parameters
-    const params = {
-      TableName: process.env.DYNAMODB_TABLE,
-      Key: {
-        id: event.pathParameters.id,
-      },
-      ExpressionAttributeNames: {
-        '#todo_text': 'text',
-      },
-      ExpressionAttributeValues: {
-        ':text': data.text,
-        ':updatedAt': timeStamp,
-      },
-      UpdateExpression: 'SET #todo_text = :text, updatedAt = :updatedAt',
-      ReturnValues: 'ALL_NEW',
-    };
-
-    // Update item
-    dynamoDb.update(params, (error, result) => {
-      if (error) {
-        console.error(error);
+    if (data.text) {
+      console.log('typeof data.text ->', typeof data.text);
+      // Check for valid text
+      if (data.text && typeof data.text !== 'string') {
+        console.log('data ->', data);
+        console.log('data.text ->', data.text);
+        console.error('Invalid data');
         callback(null, {
-          statusCode: error.statusCode,
-          headers: {
-            'Content-Type': 'text/plain',
-          },
-          body: 'Couldn\'t retrieve todo item'
+          statusCode: 400,
+          headers: { 'Content-Type': 'text/plain' },
+          body: 'Text data is not a string',
         });
         return;
       }
+      // Update text
+      function updateText(data) {
 
-      const response = {
-        statusCode: 200,
-        body: JSON.stringify(result.Attributes),
+      }
+
+      // DynamoDB parameters
+      const params = {
+        TableName: process.env.DYNAMODB_TABLE,
+        Key: {
+          id: event.pathParameters.id,
+        },
+        ExpressionAttributeNames: {
+          '#todo_text': 'text',
+        },
+        ExpressionAttributeValues: {
+          ':text': data.text,
+          ':updatedAt': timeStamp,
+        },
+        UpdateExpression: 'SET #todo_text = :text, updatedAt = :updatedAt',
+        ReturnValues: 'ALL_NEW',
       };
 
-      callback(null, response);
-    });
+      // Update item
+      dynamoDb.update(params, (error, result) => {
+        if (error) {
+          console.error(error);
+          callback(null, {
+            statusCode: error.statusCode,
+            headers: {
+              'Content-Type': 'text/plain',
+            },
+            body: 'Couldn\'t retrieve text data'
+          });
+          return;
+        }
 
-    // Update check status
-    // Check for valid "check"
-    if (typeof data.checked !== 'boolean') {
-      console.log('data ->', data);
-      console.log('data.checked ->', data.checked);
-      console.error('Invalid data');
-      callback(null, {
-        statusCode: 400,
-        headers: {
-          'Content-Type': 'text/plain'
-        },
-        body: 'Couldn\'t update todo item',
+        const response = {
+          statusCode: 200,
+          body: JSON.stringify(result.Attributes),
+        };
+
+        callback(null, response);
       });
-      return;
-    }
-    // Update checked
-    function updatechecked(data) {
-
     }
 
-    // DynamoDB parameters
-    const checked_params = {
-      TableName: process.env.DYNAMODB_TABLE,
-      Key: {
-        id: event.pathParameters.id,
-      },
-      ExpressionAttributeValues: {
-        ':checked': data.checked,
-        ':updatedAt': timeStamp,
-      },
-      UpdateExpression: 'SET checked = :checked, updatedAt = :updatedAt',
-      ReturnValues: 'ALL_NEW',
-    };
 
-    // Update item
-    dynamoDb.update(checked_params, (error, result) => {
-      if (error) {
-        console.error(error);
+    if (data.checked) {
+      // Update check status
+      // Check for valid "check"
+      if (data.checked && typeof data.checked !== 'boolean') {
+        console.log('data ->', data);
+        console.log('data.checked ->', data.checked);
+        console.error('Invalid data');
         callback(null, {
-          statusCode: error.statusCode,
-          headers: {
-            'Content-Type': 'text/plain',
-          },
-          body: 'Couldn\'t retrieve todo item'
+          statusCode: 400,
+          headers: { 'Content-Type': 'text/plain' },
+          body: 'Checked data is not a boolean',
         });
         return;
       }
+      // Update checked
+      function updatechecked(data) {
 
-      const response = {
-        statusCode: 200,
-        body: JSON.stringify(result.Attributes),
+      }
+
+      // DynamoDB parameters
+      const checked_params = {
+        TableName: process.env.DYNAMODB_TABLE,
+        Key: {
+          id: event.pathParameters.id,
+        },
+        ExpressionAttributeValues: {
+          ':checked': data.checked,
+          ':updatedAt': timeStamp,
+        },
+        UpdateExpression: 'SET checked = :checked, updatedAt = :updatedAt',
+        ReturnValues: 'ALL_NEW',
       };
 
-      callback(null, response);
-    });
+      // Update item
+      dynamoDb.update(checked_params, (error, result) => {
+        if (error) {
+          console.error(error);
+          callback(null, {
+            statusCode: error.statusCode,
+            headers: {
+              'Content-Type': 'text/plain',
+            },
+            body: 'Couldn\'t retrieve checked data'
+          });
+          return;
+        }
+
+        const response = {
+          statusCode: 200,
+          body: JSON.stringify(result.Attributes),
+        };
+
+        callback(null, response);
+      });
+
+    }
 
   };
 
